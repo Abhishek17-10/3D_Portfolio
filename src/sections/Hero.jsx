1091aae702c6b4274f91ac5cd32ea66e6bfc5db7
Abhishek <abhi007sagar@gmail.com>
2025-01-05 23:33:44 +0530

    Added 3D Canavs and Position adjustment

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,28 @@
 import {Canvas} from "@react-three/fiber";
 import {PerspectiveCamera} from "@react-three/drei";
+import {Suspense} from "react";
+import {HackerRoom} from "../components/HackerRoom.jsx";
+import {CanvasLoader} from "../components/CanvasLoader.jsx";
+import {Leva, useControls} from "leva";
 
 const Hero = () => {
+    const x = useControls("HackerRoom", {
+        positionX:{
+            value: 2.5,
+            min: -10,
+            max: 10
+        },
+        positionY:{
+            value: 2.5,
+            min: -10,
+            max: 10
+        },
+        positionZ:{
+            value: 2.5,
+            min: -10,
+            max: 10
+        },
+    })
     return (
         <section className="min-h-screen w-full flex flex-col relative">
             <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3 ">
@@ -13,8 +34,17 @@ const Hero = () => {
             </div>
 
             <div className="w-full h-full absolute inset-0">
-                <Canvas className="w-full h-full absolute inset-0">
+                <Leva/>
+                <Canvas className="w-full h-full">
+                    <Suspense fallback={<CanvasLoader/>}>
                     <PerspectiveCamera makeDefault position={[0,0,30]}/>
+                    <HackerRoom //scale={0.5}
+                                position={[0,0,0]}
+                                rotation={[0, 280, 0]}
+                    scale={[x.positionX, x.positionY, x.positionZ]}/>
+                        <ambientLight intensity={1}/>
+                        <directionalLight position={[10,10,10]} intensity={0.5}/>
+                    </Suspense>
                 </Canvas>
             </div>
         </section>
