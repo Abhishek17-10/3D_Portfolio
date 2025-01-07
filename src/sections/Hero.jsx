19ac14143d3a4a9e7be0df7059837c75e0329310
Abhishek <abhi007sagar@gmail.com>
2025-01-07 13:08:19 +0530

    Position Adjustment to 3d model

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -22,13 +22,33 @@ const Hero = () => {
             min: -10,
             max: 10
         },
+        rotationX:{
+            value: 0,
+            min: -10,
+            max: 10
+        },
+        rotationY:{
+            value: 0,
+            min: -10,
+            max: 10
+        },
+        rotationZ:{
+            value: 0,
+            min: -10,
+            max: 10
+        },
+        scale: {
+            value: 1,
+            min: 0.1,
+            max: 10
+        }
     })
     return (
         <section className="min-h-screen w-full flex flex-col relative">
             <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3 ">
                 <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">Hi, I am
                     Abhishek<span className="waving-hand">👋</span>
-                    <p className="hero_tag text-gray-gradient">Building Websites, Animations & Designs</p>
+                    <p className="hero_tag text-gray-gradient">Building Websites & Animations</p>
                 </p>
 
             </div>
@@ -38,10 +58,10 @@ const Hero = () => {
                 <Canvas className="w-full h-full">
                     <Suspense fallback={<CanvasLoader/>}>
                     <PerspectiveCamera makeDefault position={[0,0,30]}/>
-                    <HackerRoom //scale={0.5}
-                                position={[0,0,0]}
-                                rotation={[0, 280, 0]}
-                    scale={[x.positionX, x.positionY, x.positionZ]}/>
+                    <HackerRoom
+                                position={[0.9,-5.5,-4.9]}
+                                rotation={[0,-Math.PI,0 ]}
+                    scale={0.1}/>
                         <ambientLight intensity={1}/>
                         <directionalLight position={[10,10,10]} intensity={0.5}/>
                     </Suspense>
