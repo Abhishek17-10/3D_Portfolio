f3aaf9523d7b5dae8f0491fa4ec7756797b33788
Abhishek <abhi007sagar@gmail.com>
2025-01-23 23:17:59 +0530

    modified About section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,3 +1,6 @@
+import Globe from "react-globe.gl";
+import Button from "../components/Button.jsx";
+
 const About = () => {
     return (
         <section className="c-space my-20">
@@ -23,7 +26,25 @@ const About = () => {
                 <div className="col-span-1 xl:row-span-4">
                     <div className="grid-container">
                         <div className="rounded-3xl w-full sm:-[326px] h-fit flex justify-center items-center">
-
+                            <Globe height={326} width={326} backgroundColor="rgba(0,0,0,0)" backgroundImageOpacity={0.5} showAtmosphere showGraticules globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg" bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png" labelsData={[
+                                {lat: 27, lng: 74, text:"I'm here", color:'white', size:30,}
+                            ]} />
+                        </div>
+                        <div>
+                            <p className="grid-headtext">
+                                I work remotely  across most timezones
+                            </p>
+                            <p className="grid-subtext">I'm based in India, with remote work available</p>
+                            <Button name="Contact Me" isBeam containerClass="w-full mt-10"/>
+                        </div>
+                    </div>
+                </div>
+                <div className="xl:col-span-2 xl:row-span-3">
+                    <div className="grid-container">
+                        <img src="/assets/grid3.png" alt="grid-3" className="w-full sm:h-[266px] h-fit object-contain"/>
+                        <div>
+                            <p className="grid-headtext">My passion for Coding</p>
+                            <p className="grid-subtext">I love solving problems and building things through code</p>
                         </div>
                     </div>
                 </div>
