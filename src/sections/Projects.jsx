93861a74d8b350b068bbb0cdfe7ca2646353b5f4
Abhishek <abhi007sagar@gmail.com>
2025-01-26 20:21:04 +0530

    modified

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import {myProjects} from "../Constants/index.js";
 
 export const Projects = () => {
     return (
-        <div>Projects</div>
+        <section className="c-space my-20">
+            <p className="head-text"> My Projects</p>
+            <div className="grid lg:grid-cols-2 grid-cols-1 mt-12 gap-5 w-full">
+                <div className="flex flex-col gap-5 relative sm:p-10 py-10 px-5 shadow-2xl shadow-black-200 ">
+                    <div className="absolute top-0 right-0">
+                        <img src={myProjects[0].spotlight} alt="spotlight" className="w-full h-96  rounded-xl"/>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
