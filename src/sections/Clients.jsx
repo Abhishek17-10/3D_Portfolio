e57a16baa03b73b0addb501961c18ef6109ed60e
Abhishek <abhi007sagar@gmail.com>
2025-02-20 19:41:03 +0530

    Updated

--- src/sections/Clients.jsx
+++ src/sections/Clients.jsx
@@ -9,6 +9,15 @@ export const Clients = () => {
                     <div key={id} className="client-review">
                         <div>
                             <p className="text-white font-light">{review}</p>
+                            <div className="client-content">
+                                <div className="flex- gap-3">
+                                    <img src={img} alt={name} className="w-12 h-12 rounded-full"/>
+                                    <div className="flex flex-col">
+                                        <p className="font-semibold text-white-800">{name}</p>
+                                        <p className="text-white-500 md:text-base text-sm">{position}</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 ))}
