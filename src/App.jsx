537c4620ee929e712874d557a2de9dab199f5938
Abhishek <abhi007sagar@gmail.com>
2025-02-21 14:06:54 +0530

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./sections/Hero.jsx";
 import About from "./sections/About.jsx";
 import {Projects} from "./sections/Projects.jsx";
 import {Clients} from "./sections/Clients.jsx";
+import Contact from "./sections/Contact.jsx";
  const App = () => {
   return (
       <main className= "max-w-7xl mx-auto">
@@ -11,6 +12,8 @@ import {Clients} from "./sections/Clients.jsx";
           <About/>
           <Projects/>
           <Clients/>
+
+          <Contact/>
       </main>
   );
  }
